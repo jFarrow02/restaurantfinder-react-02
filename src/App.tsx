a38e459b8d2558ba6f005282b7f99399e2022d5b
jFarrow02 <jack.dempsey.farrow@gmail.com>
2021-07-11 15:27:01 -0400

    creates restaurant details component and adds router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
+import React from 'react';
 import './App.scss';
 import LandingPage from './components/LandingPage/LandingPage';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>NY RestaurantFinder</h1>
-      <LandingPage/>
-    </div>
-  );
+class App extends React.Component<{}, {}> {
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <h1>NY RestaurantFinder</h1>
+           <Switch>
+            <Route path="/">
+              <LandingPage/>
+            </Route>
+            <Route path="/search">
+              <LandingPage/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+  
 }
 
 export default App;
